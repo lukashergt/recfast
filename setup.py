fc5c602b6d8534262fa07a0bb2c850262871ff3e
lukashergt <lh561@mrao.cam.ac.uk>
2021-08-04 11:56:04 -0700

    clean-up setup.py

--- setup.py
+++ setup.py
@@ -20,11 +20,3 @@ setup(
     define_macros=[('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')]
 )
 
-# setup(
-#     name='pyrecfast',
-#     ext_modules=cythonize([Extension("pyrecfast", ["pyrecfast.pyx"], extra_link_args=["pyrecfast.o"])],
-#                           compiler_directives={'language_level': "3"}),
-#     zip_safe=False,
-#     include_dirs=[np.get_include()]
-# )
-
