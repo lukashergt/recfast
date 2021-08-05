c14d04d09e07f0583aff8d551b404fc5e0bbc2e4
lukashergt <lh561@mrao.cam.ac.uk>
2021-08-04 20:41:43 -0700

    remove sys path insertion from tests, not needed since tests started from package directory

--- test/test_example_data.py
+++ test/test_example_data.py
@@ -1,4 +1,3 @@
-import sys
 import numpy as np
 from numpy.testing import assert_array_equal
 
@@ -6,17 +5,15 @@ from numpy.testing import assert_array_equal
 data = "test/example_data/example_new_CODATA_AME_2photon_ionisationlevel.out"
 
 
-def test_example_data():
+def test_recfast_out_to_example_data():
     z_data, x_data = np.loadtxt(data, unpack=True)
     z, x = np.loadtxt("test.out", unpack=True)
     assert_array_equal(z, z_data)
     assert_array_equal(x, x_data)
 
 
-def test_pyrecfast():
-    sys.path.insert(0, "/home/hergtl/Documents/Projects/RecFastPrj/recfast/")
+def test_pyrecfast_to_example_data():
     from pyrecfast import recfast
-
     z_data, x_data = np.loadtxt(data, unpack=True)
     z, x = recfast(OmegaB=0.04,
                    OmegaC=0.20,
