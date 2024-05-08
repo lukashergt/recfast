de1cc02da8d5a451d98f5007930374d563e30bf9
lukashergt <lthergt@phas.ubc.ca>
2024-05-08 00:53:31 -0700

    add package data to `setup.py`

--- setup.py
+++ setup.py
@@ -15,8 +15,11 @@ compiler_directives = {'language_level': 3}
 
 setup(
     name='pyrecfast',
-    ext_modules=cythonize(extensions, **compiler_directives),
+    #ext_modules=cythonize(extensions, **compiler_directives),
     include_dirs=[np.get_include()],
-    define_macros=[('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')]
+    define_macros=[('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')],
+    packages=['pyrecfast'],
+    package_dir={'pyrecfast': 'pyrecfast'},
+    package_data={'pyrecfast': ['pyrecfast.so']},
 )
 
